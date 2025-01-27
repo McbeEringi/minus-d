d2f5f7113021cba81dc631119b9a52dec1037a92
McbeEringi <git@mcbeeringi.dev>
2025-01-28 01:38:31 +0900

    -

--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,7 @@ map=(w,f)=>Object.entries(w).reduce((a,[i,x])=>(a[i]=f(x),a),{}),
 length=w=>(w.x*w.x+w.y*w.y+w.z*w.z)**.5,
 distance=(a,b)=>length(sub(a,b)),
 dot=(a,b)=>Object.values(mul(a,b)).reduce((a,x)=>a+x),
+smin=(a,b,k)=>(k/=1-.5**.5,(x=>b-k*(1<x?x:x<-1?0:1+.5*(x-(2-x*x)**.5)))((b-a)/k)),
 
 draw=async({
 	size={x:8,y:8,z:8},
@@ -21,21 +22,20 @@ draw=async({
 	place=false,
 	msg=false,
 	infill=true,
-	id=`toko-ess:${Math.floor(Math.random()*0xffffffff).toString(16).padStart(8,0)}`,
+	id=`minus-d:${Math.floor(Math.random()*0xffffffff).toString(16).padStart(8,0)}`,
 	hc,hcl
 }={})=>(
 	place&&(place={d:place.dimension,p:place.location}),
 	hc=map(chunk,x=>x*.5),hcl=dot(hc,hc)**.5,
 	
-	await[...Array(Math.ceil(size.y/chunk.y))].reduce(async(a,y,j,{length:l})=>(
-		y=(j==l-1?size.y%chunk.y:chunk.y),
+	await[...Array(Math.ceil(size.y/chunk.y))].reduce(async(a,y,j,{length:l},o={})=>(
+		y=(j==l-1?size.y-(o.y=j*chunk.y):chunk.y),
 		a=await a,
 		msg&&msg.sendMessage(j?`${id} ${j}/${l} done...`:`${id} render started...`),
 		a.concat(await[...Array(Math.ceil(size.z/chunk.z))].reduce(async(a,z,k,{length:l})=>(
-			z=(k==l-1?size.z%chunk.z:chunk.z),
-			(await a).concat(await[...Array(Math.ceil(size.x/chunk.x))].reduce(async(a,s,i,{length:l},o)=>(
-				s={x:(i==l-1?size.x%chunk.x:chunk.x),y,z},
-				o={x:i*chunk.x,y:j*chunk.y,z:k*chunk.z},
+			z=(k==l-1?size.z-(o.z=k*chunk.z):chunk.z),
+			(await a).concat(await[...Array(Math.ceil(size.x/chunk.x))].reduce(async(a,s,i,{length:l})=>(
+				s={x:(i==l-1?size.x-(o.x=(i*chunk.x):chunk.x),y,z},
 				sdf(add(o,hc))<=hcl&&(infill||-hcl<=sdf(add(o,hc)))&&(await a).push(await(async w=>(
 					await run(_=>(
 					[...Array(s.y)].reduce((a,_,y)=>(
@@ -82,7 +82,7 @@ world.beforeEvents.chatSend.subscribe((
 			},{place:p,msg:p}),
 			p.sendMessage(`[  §aOK§r  ] sphere: Created sphere(r=${r}).`)
 		)):p.sendMessage(`[§cFAILED§r] sphere: "${r}" is NaN or falsy value.`),
-		octa:r=>+r?run(async()=>(
+		octa:r=>+r?run(async w=>(
 			r=+r,
 			w=p=>dot(map(sub(p,(x=>({x,y:x,z:x}))(r)),x=>Math.abs(x)),{x:1,y:1,z:1})-r,
 			await draw({
@@ -92,6 +92,17 @@ world.beforeEvents.chatSend.subscribe((
 			},{place:p,msg:p}),
 			p.sendMessage(`[  §aOK§r  ] octa: Created octa(r=${r}).`)
 		)):p.sendMessage(`[§cFAILED§r] octa: "${r}" is NaN or falsy value.`),
+		smin:k=>run(async()=>(
+			await draw({
+				size:{x:8,y:20,z:8},
+				sdf:p=>smin(
+					distance(p,{x:4,y: 4,z:4})-4,
+					distance(p,{x:4,y:16,z:4})-4,
+					+k
+				),
+			},{place:p,msg:p}),
+			p.sendMessage(`[  §aOK§r  ] smin: Created smin(k=${k}).`)
+		)),
 		info:x=>p.sendMessage(JSON.stringify(d.getBlock(p.location).permutation.getAllStates())),
 		s:x=>run({
 			l:_=>p.sendMessage(JSON.stringify(world.structureManager.getWorldStructureIds())),
